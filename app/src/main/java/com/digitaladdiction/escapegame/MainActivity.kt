7375c653dad9ca9111c8dd40e8ac6493a0eb687d
its-r-k <rajesh.pandey1@s.amity.edu>
2026-04-26 01:15:50 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/digitaladdiction/escapegame/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.digitaladdiction.escapegame
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            allowFileAccessFromFileURLs = true
+            allowUniversalAccessFromFileURLs = false
+            domStorageEnabled = true
+            cacheMode = WebSettings.LOAD_NO_CACHE
+            builtInZoomControls = false
+            displayZoomControls = false
+            setSupportZoom(false)
+            useWideViewPort = true
+            loadWithOverviewMode = true
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                return !request.url.scheme.equals("file", ignoreCase = true)
+            }
+        }
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack()
+        else super.onBackPressed()
+    }
+}
